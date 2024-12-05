7f3bf7a4fd5b607e910e2186748610e602238b04
Ben <ben>
2024-12-05 14:32:30 -0500

    Restructure and add JS placeholder

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -6,10 +6,10 @@ namespace AdventOfCode2024
     {
         public static void Main(string[] args)
         {
-            string day1Input = File.ReadAllText("./Inputs/Day1.txt");
-            Console.WriteLine(Day1.GetListDistance(day1Input));
+            string day1Input = File.ReadAllText("../inputs/Day1.txt");
+            Console.WriteLine($"Day 1 answer: {Day1.GetListDistance(day1Input)}");
             //We can use Day 1's input for Day 2 (no changes).
-            Console.WriteLine(Day2.GetListOverlap(day1Input));
+            Console.WriteLine($"Day 2 answer: {Day2.GetListOverlap(day1Input)}");
         }
     }
 }
