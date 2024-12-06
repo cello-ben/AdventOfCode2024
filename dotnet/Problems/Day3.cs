23976f6f4f05e81b67328da044c01ef424438657
Ben <ben>
2024-12-06 14:42:08 -0500

    Day 3, part 2

--- dotnet/Problems/Day3.cs
+++ dotnet/Problems/Day3.cs
@@ -4,10 +4,48 @@ namespace AdventOfCode2024.Problems
 {
     public class Day3
     {
+        public static int DecodeCorruptedMemoryWithDoDont(string memory)
+        {
+            int res = 0;
+            bool canDo = true;
+            Regex matchRegex = new(@"(mul\([0-9]+,[0-9]+\))|do\(\)|don't\(\)");
+            foreach (Match match in matchRegex.Matches(memory))
+            {
+                Console.WriteLine(match);
+                switch (match.Value.ToString())
+                {
+                    case "do()":
+                        canDo = true;
+                        break;
+                    case "don't()":
+                        canDo = false;
+                        break;
+                    default:
+                        if (canDo)
+                        {
+                            string[] pair = match.Value.ToString().Split(",");
+                            int product = 1;
+                            foreach (string s in pair)
+                            {
+                                IEnumerable<char> numEnum = s.Where(c => char.IsDigit(c));
+                                string num = String.Join("", numEnum);
+                                bool canParse = int.TryParse(num, out int n);
+                                if (canParse)
+                                {
+                                    product *= n;
+                                }
+                            }
+                            res += product;
+                        }
+                        break;
+                }
+            }
+            return res;
+        }
         public static int DecodeCorruptedMemory(string memory)
         {
             int res = 0;
-            Regex matchRegex = new(@"mul\([0-9]+,[0-9]+\)");
+            Regex matchRegex = new(@"(mul\([0-9]+,[0-9]+\))|do\(\)|don't\(\)");
             foreach (Match match in matchRegex.Matches(memory))
             {
                 string[] pair = match.Value.ToString().Split(",");
--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -11,9 +11,10 @@ namespace AdventOfCode2024
             Console.WriteLine($"Day 1 Part 2 answer: {Day1.GetListOverlap(day1Input)}");
             string day2Input = File.ReadAllText("../inputs/Day2.txt");
             Console.WriteLine($"Day 2 Part 1 answer: {Day2.GetSafeReports(day2Input)}");
-            string day3Input = File.ReadAllText("../inputs/Day3.txt");
             Console.WriteLine($"Day 2 Part 2 Answer: {Day2.GetSafeReportsWithRemoval(day2Input)}");
+            string day3Input = File.ReadAllText("../inputs/Day3.txt");
             Console.WriteLine($"Day 3 Part 1 answer: {Day3.DecodeCorruptedMemory(day3Input)}");
+            Console.WriteLine($"Day 3 Part 2 answer; {Day3.DecodeCorruptedMemoryWithDoDont(day3Input)}");
         }
     }
 }
